90a79bd5bc9a664fee9f738b0a41be55cf9e035c
Nevada <kingiwhata@gmail.com>
2022-04-14 02:22:56 +1200

    tracks setup

--- src/App.svelte
+++ src/App.svelte
@@ -25,14 +25,16 @@
 		let room = connection.initJitsiConference("21e8call", {})
 		room.on(JitsiMeetJS.events.conference.TRACK_ADDED, onTrackAdded);
 		room.on(JitsiMeetJS.events.conference.CONFERENCE_JOINED, async () => {
-    		const tracks = await JitsiMeetJS.createLocalTracks({devices: ['video', 'audio']})
+    		const tracks = await JitsiMeetJS.createLocalTracks({devices: ['audio']})
 			tracks.forEach(track => {
 				room.addTrack(track)
+				console.log(track.getType())
 				tracksArr = [...tracksArr, track]
+           		track.attach(document.getElementById("aud"));
 			});
 		})
 		room.join(null, null)
-		window.open(roomURL, "POPUP", "height=800,width=800,left=100,top=100")
+		// window.open(roomURL, "POPUP", "height=800,width=800,left=100,top=100")
 	}
 
 	const onDisconnect = () => {
@@ -51,6 +53,9 @@
 
 <main>
 	<h1>JITSI 21e8</h1>
+	<div class="grid-div">
+		<audio autoplay="1" muted="true" id="aud"></audio>
+	</div>
 	<button on:click={() => connection.connect({})}>Join Room</button>
 	<button on:click={() => connection.disconnect({})}>Leave Room</button>
 </main>
@@ -66,8 +71,15 @@
 	h1 {
 		color: #ff3e00;
 		text-transform: uppercase;
-		font-size: 4em;
+		font-size: 3em;
 		font-weight: 100;
+	}
+
+	.grid-div {
+		display: grid;
+		border: 1px solid black;
+		height: calc(100vh - 30vh);
+		margin-bottom: 1em;
 	}
 
 	@media (min-width: 640px) {
