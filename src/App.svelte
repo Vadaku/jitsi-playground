d85535b887146bf2282562f993f7217237d7b2a5
Vadaku <kingiwhata@gmail.com>
2022-04-19 17:07:44 +1200

    custom ui

--- src/App.svelte
+++ src/App.svelte
@@ -1,66 +1,134 @@
 <script>
 	import JitsiMeetJS from "@lyno/lib-jitsi-meet";
 
+    export let data;
+
 	const connOptions = {
 		hosts: {
 			domain: "meet.jit.si",
 			muc: "conference.meet.jit.si"
 		},
-		bosh: "https://meet.jit.si/http-bind"
+		serviceUrl: "https://meet.jit.si/http-bind"
 	}
 
-	let tracksArr = [];
-	
+    let room = null;
+	let localTracks = [];
+	let remoteTracks = [];
+    let showVideo = false;
+
 	JitsiMeetJS.setLogLevel(JitsiMeetJS.logLevels.ERROR)
 
 	const initOptions = {}
 
-	const onTrackAdded = () => {
-		console.log("TRACK ADDED")
+	const onRemoteTrack = (track) => {
+		console.log("Track being added")
+		if (track.isLocal()) {
+			return;
+		}
+		track.forEach((track, i) => {
+			room.addTrack(track)
+			if (track.getType() == "video") {
+				let vidContainer = document.getElementById("remote-con")
+				let vid = document.createElement("video")
+				vid.setAttribute(`id`, `remote-video${i}`)
+				vid.setAttribute(`height`, `500`)
+				vid.setAttribute(`width`, `500`)
+				vid.setAttribute(`autoplay`, 1)
+				vidContainer.append(vid)
+				track.attach(vid);
+			} else {
+				let audContainer = document.getElementById("remote-con")
+				let aud = document.createElement("audio")
+				aud.setAttribute(`id`, `remote-audio${i}`)
+				aud.setAttribute(`autoplay`, 1)
+				audContainer.append(aud)
+				track.attach(aud);
+			}
+		});
 	}
 
+    const onLocalTracks = (tracks) => {
+		console.log(tracks)
+		tracks.forEach((track, i) => {
+			// room.addTrack(track)
+			if (track.getType() == "video") {
+                    let vidContainer = document.getElementById("con")
+					let vid = document.createElement("video")
+					vid.setAttribute(`id`, `local-video${i}`)
+					vid.setAttribute(`autoplay`, 1)
+					vid.setAttribute(`height`, `500`)
+					vid.setAttribute(`width`, `500`)
+					vidContainer.append(vid)
+					track.attach(vid);
+				} else {
+					let audContainer = document.getElementById("con")
+					let aud = document.createElement("audio")
+					aud.setAttribute(`id`, `local-audio${i}`)
+					aud.setAttribute(`autoplay`, 1)
+					audContainer.append(aud)
+					track.attach(aud);
+				}
+		})
+    }
+
 	const onConnectionSuccess = () => {
-		let roomURL = "https://" + connOptions.hosts.domain + "/21e8call"
+		let roomURL = "https://" + connOptions.hosts.domain + "/" + data
 		console.log("Connection Successful")
-		let room = connection.initJitsiConference("21e8call", {})
-		room.on(JitsiMeetJS.events.conference.TRACK_ADDED, onTrackAdded);
-		room.on(JitsiMeetJS.events.conference.CONFERENCE_JOINED, async () => {
-    		const tracks = await JitsiMeetJS.createLocalTracks({devices: ['audio']})
-			tracks.forEach(track => {
-				room.addTrack(track)
-				console.log(track.getType())
-				tracksArr = [...tracksArr, track]
-           		track.attach(document.getElementById("aud"));
-			});
+		room = connection.initJitsiConference("21e8call", {})
+		room.on(JitsiMeetJS.events.conference.TRACK_ADDED, onRemoteTrack);
+		room.on(JitsiMeetJS.events.conference.CONFERENCE_JOINED, () => {
+    		console.log("Conference Joined")
+		})
+		room.on(JitsiMeetJS.events.conference.USER_JOINED, () => {
+			console.log("User Joined")
 		})
 		room.join(null, null)
-		// window.open(roomURL, "POPUP", "height=800,width=800,left=100,top=100")
+		// window.open(roomURL, "POPUP", "fullscreen=yes")
 	}
 
 	const onDisconnect = () => {
-		console.log("Disconnected")
+		console.log("Jitsi Disconnected")
+		connection.removeEventListener(JitsiMeetJS.events.connection.CONNECTION_ESTABLISHED, onConnectionSuccess)
+		connection.removeEventListener(JitsiMeetJS.events.connection.CONNECTION_FAILED, () => {
+			console.log("Jitsi Disconnected")
+		})
+		connection.removeEventListener(JitsiMeetJS.events.connection.CONNECTION_DISCONNECTED, onDisconnect)
 	}
 
 	JitsiMeetJS.init(initOptions)
 	
+    connOptions.serviceUrl = connOptions.serviceUrl + "?room=21e8call"
 	let connection = new JitsiMeetJS.JitsiConnection(null, null, connOptions);
 	connection.addEventListener(JitsiMeetJS.events.connection.CONNECTION_ESTABLISHED, onConnectionSuccess);
+	connection.addEventListener(JitsiMeetJS.events.connection.CONNECTION_FAILED, () => {
+		console.log("Connection to Jitsi failed")
+	});
 	connection.addEventListener(JitsiMeetJS.events.connection.CONNECTION_DISCONNECTED, onDisconnect);
-
+	connection.connect()
 	
-	// JitsiMeetJS.createLocalTracks().then(onLocalTracks);
+	// JitsiMeetJS.createLocalTracks({devices: ["audio", "video"]}).then(onLocalTracks);
 </script>
 
 <main>
 	<h1>JITSI 21e8</h1>
-	<div class="grid-div">
-		<audio autoplay="1" muted="true" id="aud"></audio>
+	Local Video
+	<div id="con">
+		<!-- svelte-ignore a11y-media-has-caption -->
+        	<!-- <video autoplay="1" id="vid" width="500" height="500"></video>
+			<audio autoplay="1" muted="true" id="aud"></audio> -->
 	</div>
-	<button on:click={() => connection.connect({})}>Join Room</button>
-	<button on:click={() => connection.disconnect({})}>Leave Room</button>
+	Remote Video
+	<div id="remote-con">
+		<!-- svelte-ignore a11y-media-has-caption -->
+        	<!-- <video autoplay="1" id="vid" width="500" height="500"></video>
+			<audio autoplay="1" muted="true" id="aud"></audio> -->
+	</div>
+	<button on:click={() => {connection.connect();}}>Join Room</button>
+	<button on:click={() => {connection.disconnect({}); showVideo = false;}}>Leave Room</button>
 </main>
 
 <style>
+
 	main {
 		text-align: center;
 		padding: 1em;
@@ -75,8 +143,15 @@
 		font-weight: 100;
 	}
 
-	.grid-div {
-		display: grid;
+	#con {
+		display: flex;
+		border: 1px solid black;
+		height: calc(100vh - 30vh);
+		margin-bottom: 1em;
+	}
+
+	#remote-con {
+		display: flex;
 		border: 1px solid black;
 		height: calc(100vh - 30vh);
 		margin-bottom: 1em;
